9fefc8bba7e6cebd88f08d3630a98050048a3b62
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-03-10 10:13:39 +0100

    changed uri + removed foaf:page for besluit. Ignore articles for now

--- lib/semantic-model-generator.rb
+++ lib/semantic-model-generator.rb
@@ -6,7 +6,7 @@ class SemanticModelGenerator
   BESLUIT = RDF::Vocabulary.new("http://data.vlaanderen.be/ns/besluit#")
   ELI = RDF::Vocabulary.new("http://data.europa.eu/eli/ontology#")
   MU = RDF::Vocabulary.new("http://mu.semte.ch/vocabularies/core/")
-  
+
   LANG_NL = RDF::URI.new("http://publications.europa.eu/resource/authority/language/NLD")
 
   def initialize
@@ -14,7 +14,7 @@ class SemanticModelGenerator
   end
 
   def insert_besluit(document)
-    subject = RDF::URI(document["Link"]["Href"])
+    subject = construct_document_uri(document)
 
     id = document["Id"]
     uuid = BSON::ObjectId.new.to_s
@@ -26,21 +26,20 @@ class SemanticModelGenerator
     citeeropschrift = if match then match[1] else title end
 
     @graph << RDF.Statement(subject, RDF.type, BESLUIT.Besluit)
-    @graph << RDF.Statement(subject, MU.uuid, uuid)    
+    @graph << RDF.Statement(subject, MU.uuid, uuid)
     @graph << RDF.Statement(subject, ELI["date_publication"], RDF::Literal.new(date, datatype: RDF::XSD.date))
     @graph << RDF.Statement(subject, ELI.title, title)
     @graph << RDF.Statement(subject, ELI["title_short"], citeeropschrift)
     # TODO replace with correct documentType URI
     @graph << RDF.Statement(subject, ELI["type_document"], CODEX["WetgevingDocumentType/#{doc_type}"])
     @graph << RDF.Statement(subject, ELI.language, LANG_NL)
-    @graph << RDF.Statement(subject, RDF::Vocab::FOAF.page, RDF::URI.new("https://codex.vlaanderen.be/Zoeken/Document.aspx?DID=#{id}&param=inhoud"))
 
     { id: id, uri: subject }
   end
 
   def enrich_besluit(document_detail)
     document = document_detail["Document"]
-    subject = RDF::URI(document["Link"]["Href"])
+    subject = construct_document_uri(document)
 
     if document["StartDatum"]
       startDate = document["StartDatum"][0, 10] # "YYYY-MM-DD" substring
@@ -51,7 +50,7 @@ class SemanticModelGenerator
       @graph << RDF.Statement(subject, ELI["date_no_longer_in_force"], RDF::Literal.new(endDate, datatype: RDF::XSD.date))
     end
 
-    { id: document["Id"], uri: subject }    
+    { id: document["Id"], uri: subject }
   end
 
   def insert_article(article, besluit)
@@ -59,9 +58,9 @@ class SemanticModelGenerator
 
     id = article["Id"]
     uuid = BSON::ObjectId.new.to_s
-    
+
     @graph << RDF.Statement(subject, RDF.type, BESLUIT.Artikel)
-    @graph << RDF.Statement(subject, MU.uuid, uuid)    
+    @graph << RDF.Statement(subject, MU.uuid, uuid)
     @graph << RDF.Statement(subject, ELI.title, "Artikel #{article["Titel"]}".strip)
     @graph << RDF.Statement(subject, ELI.number, article["Titel"])
     @graph << RDF.Statement(subject, ELI.language, LANG_NL)
@@ -70,7 +69,11 @@ class SemanticModelGenerator
 
     { id: id, uri: subject }
   end
-  
+
+  def construct_document_uri(document)
+    RDF::URI(document["Link"]["Href"] + '/volledigdocument')
+  end
+
   def write_ttl_to_file(file)
     RDF::Writer.open(file) { |writer| writer << @graph }
     # graph.to_ttl
